35000598ad7cc0db1dba75be1a8567829a98109d
Mohammed Irfan <mohammedirfanrj@gmail.com>
2024-06-12 15:23:08 +0530

    DB Issues

--- Frontend/src/components/Hero/Hero.jsx
+++ Frontend/src/components/Hero/Hero.jsx
@@ -12,7 +12,7 @@ function Hero() {
     <img src="https://th.bing.com/th/id/R.c0074b70b41a52a64c7a918fcf35acd7?rik=%2bIx7I1fcwqp8EA&riu=http%3a%2f%2f1.bp.blogspot.com%2f-fgw48tQoohY%2fUuvZ_GUr7wI%2fAAAAAAAAFfk%2faSfXJkNx5Cg%2fs1600%2fWedding%2bCake%2bHd%2bPhoto%2bGallery%2b(32).jpg&ehk=pX8GYHOZivBCCiX%2fIO0JT%2fG3UlwtCQWpzDUXd5%2bB%2flg%3d&risl=&pid=ImgRaw&r=0" className="max-w-56 rounded-lg shadow-2xl" />
     <div>
       <h1 className="text-5xl font-bold">We Bake You Enjoy</h1>
-      <p className="py-6">Provident cupiditate voluptatem et in. Quaerat fugiat ut assumenda excepturi exercitationem quasi. In deleniti eaque aut repudiandae et a id nisi.</p>
+      <p className="py-6">Baked with love, crafted by hand.</p>
     <Link to={"/products"}>   <button className="btn btn-primary">Order Now!</button> </Link>
     </div>
   </div>
