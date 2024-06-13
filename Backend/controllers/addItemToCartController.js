26ead7c3c390447e871cba5594d3dbe9a282e928
Mohammed Irfan <mohammedirfanrj@gmail.com>
2024-06-14 04:56:13 +0530

    R

--- Backend/controllers/addItemToCartController.js
+++ Backend/controllers/addItemToCartController.js
@@ -5,26 +5,22 @@ const Cart = require("../models/cartSchema.js");
 
 
 
-const addToCart = async (req,res)=>{
+const addToCart = async (req, res) => {
 
-    let {username , password , productId} = req.body;
+    let { username, password, productId } = req.body;
     try {
-        let result = await Customer.findOneAndUpdate({username,password},{$push:{cart:productId}},{new:true});
+        let result = await Customer.findOneAndUpdate({ username, password }, { $push: { cart: productId } }, { new: true });
+        res.json({ result });
+        // let cart = result.cart;
+        // res.json({ cart, productId });
 
-        let cart = result.cart;
 
-        res.json({ cart , productId });
-    //     .then(async(result)=>{      
-    //         let cartId = result.cart;
-               
-    //         res.json({cartId,productId});
-    //     })
     } catch (error) {
-        
+
     }
 
-   
+
 
 }
 
-module.exports = {addToCart};
+module.exports = { addToCart };
