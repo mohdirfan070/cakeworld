28ee354a290c8c46ca5f3ef3bb4698b670cec13e
Mohammed Irfan <mohammedirfanrj@gmail.com>
2024-06-28 18:10:05 +0530

    UTR LInk

--- Frontend/src/pages/Cart.jsx
+++ Frontend/src/pages/Cart.jsx
@@ -108,11 +108,7 @@ let [qrurl,setQrurl]=useState("");
     <p className="pt-2">Total Charges : <span className="font-semibold">₹{ eval(`${cart.totalPrice}+40`) } </span></p>
 
     <div className="flex flex-col align-middle rounded-md justify-center w-full  border-4 p-4 border-neutral mt-3">
-<<<<<<< HEAD
    <img className="rounded-md w-52 self-center shadow-xl" src={`https://api.qrserver.com/v1/create-qr-code/?size=160x160&data=upi%3A%2F%2Fpay%3Fpa%3D9538321498%40ibl%26pn%3Dmohammed%20Irfan%26am%3D${eval(`${cart.totalPrice}+40`)}%26cu%3DINR`} alt="" />
-=======
-   <img className="rounded-xl w-52 self-center" src={`https://api.qrserver.com/v1/create-qr-code/?size=160x160&data=upi%3A%2F%2Fpay%3Fpa%3D9538321498%40ibl%26pn%3Dmohammed%20Irfan%26am%3D${eval(`${cart.totalPrice}+40`)}%26cu%3DINR`} alt="" />
->>>>>>> b5cd96dadcc2638c4f8df74c65aae15a45eeff34
    <section className="flex flex-col justify-centre py2 my-3">
    <h1 className=" rounded-md font-medium w-22  text-wrap ">Scan  with your payments app and write the <a href="https://www.inmyview.in/wp-content/uploads/2024/02/utr-number-upi-apps.jpg.webp"><b className="underline link-info">UTR</b></a>  number here</h1>
    <p className="tooltip tooltip-bottom"  data-tip="ex: 493456789101 ">
