15a73cad1e9933cd19829607e4d7af2ff92d531d
Mohammed Irfan <mohammedirfanrj@gmail.com>
2024-06-29 13:16:50 +0530

    Login Error

--- Frontend/src/pages/Login.jsx
+++ Frontend/src/pages/Login.jsx
@@ -94,7 +94,7 @@ export default function Login() {
       //  console.log(inpData);
       // s://cakeworld.onrender.com
       let result = await axios.post(
-        "http://cakeworld.onrender.com/api/signup",
+        "https://cakeworld.onrender.com/api/signup",
         inpData
       );
       setLogin(true);
@@ -124,7 +124,7 @@ export default function Login() {
       //  console.log(inpData);
       // s://cakeworld.onrender.com
       let result = await axios.post(
-        "http://cakeworld.onrender.com/api/login",
+        "https://cakeworld.onrender.com/api/login",
         inpData
       );
       setLogin(true);
